7444d565c215e99f8d674a66ddd40a6505c092c9
vedant bharad <92723979+Vedantbharad2603@users.noreply.github.com>
2023-02-14 18:11:20 +0530

    day 120

---
+++ 120Day/day120..cpp
@@ -0,0 +1,17 @@
+// 0x120Day of 0x365Days challenge
+// VEDANT BHARAD
+// 14-2-2023
+#include <iostream>
+using namespace std;
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+    while (t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        cout<<x/y<<endl;
+    }
+	return 0;
+}
