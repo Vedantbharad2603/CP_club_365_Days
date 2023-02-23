5f4054f8d98fe650acf2f5307cb1b452bb14740a
vedant bharad <92723979+Vedantbharad2603@users.noreply.github.com>
2023-02-23 22:38:49 +0530

    day 129

---
+++ 129Day/day129.cpp
@@ -0,0 +1,13 @@
+// 0x129Day of 0x365Days challenge
+// VEDANT BHARAD
+// 23-2-2023
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	cout << n << endl;
+	return 0;
+}
