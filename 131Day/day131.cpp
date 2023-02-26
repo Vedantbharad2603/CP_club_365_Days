cb956f46a2b10e5832e7a0f927ff901ff471547d
vedant bharad <92723979+Vedantbharad2603@users.noreply.github.com>
2023-02-26 16:46:46 +0530

    day 131

---
+++ 131Day/day131.cpp
@@ -0,0 +1,15 @@
+// 0x131Day of 0x365Days challenge
+// VEDANT BHARAD
+// 26-2-2023
+#include <iostream>
+using namespace std;
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if((x+7)>170)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+	return 0;
+}
