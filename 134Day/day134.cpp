7b2ca6c640b881a6fb24fb22caabd5d8415fa7fe
vedant bharad <92723979+Vedantbharad2603@users.noreply.github.com>
2023-03-02 22:10:02 +0530

    day 134 and 135

---
+++ 134Day/day134.cpp
@@ -0,0 +1,23 @@
+// 0x134Day of 0x365Days challenge
+// VEDANT BHARAD
+// 01-03-2023
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >>y;
+        if((x+y)>6)
+        {
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+	}
+	return 0;
+}
